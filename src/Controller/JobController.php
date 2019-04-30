7b0a8902c4d83439d1b2cd62817b56ee992235c9
sidczak <sidczak@clickmeeting.com>
2019-04-30 12:37:47 +0200

    Jobeet Day 4: The Controller and the View

---
+++ src/Controller/JobController.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Job;
+use Symfony\Component\HttpFoundation\Response;
+
+class JobController extends AbstractController
+{
+    /**
+     * Lists all job entities.
+     *
+     * @Route("/", name="job.list")
+     *
+     * @return Response
+     */
+    public function list() : Response
+    {
+        $jobs = $this->getDoctrine()->getRepository(Job::class)->findAll();
+
+        return $this->render('job/list.html.twig', [
+            'jobs' => $jobs,
+        ]);
+    }
+
+    /**
+     * Finds and displays a job entity.
+     *
+     * @Route("/{id}", name="job.show")
+     *
+     * @param Job $job
+     *
+     * @return Response
+     */
+    public function show(Job $job) : Response
+    {
+        return $this->render('job/show.html.twig', [
+            'job' => $job,
+        ]);
+    }
+}
