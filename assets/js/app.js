b411cf399a43bab361156b5db62f5719b41cc02d
sidczak <sidczak@clickmeeting.com>
2019-05-17 17:47:33 +0200

    add webpack bootstrap jquery

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// require jQuery normally
+var $ = require('jquery');
+
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
+require('bootstrap');
+
+$(document).ready(function() {
+  $(".dropdown-toggle").dropdown();
+});
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
