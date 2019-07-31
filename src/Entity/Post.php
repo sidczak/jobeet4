e2e5468456fed85f12573f37e80d03b834d77348
sidczak <sidczak@clickmeeting.com>
2019-07-31 17:36:34 +0200

    vue-api - Step5 - add class post

---
+++ src/Entity/Post.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+class Post
+{
+    private $id;
+
+    private $message;
+
+    private $createdAt;
+
+    private $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function prePersist()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+}
