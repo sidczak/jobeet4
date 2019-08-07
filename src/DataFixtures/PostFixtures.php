e84ccf6b8d1f24a8338bc079383d8660b33aba3f
sidczak <sidczak@clickmeeting.com>
2019-08-07 17:54:04 +0200

    dane do testowania post fixtures

---
+++ src/DataFixtures/PostFixtures.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\DataFixtures;
+
+use App\Entity\Post;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class PostFixtures extends Fixture
+{
+    /**
+     * @param ObjectManager $manager
+     *
+     * @return void
+     */
+    public function load(ObjectManager $manager) : void
+    {
+        $post1 = new Post();
+        $post1->setMessage('Post1 Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
+
+        $post2 = new Post();
+        $post2->setMessage('Post2 Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
+
+        $manager->persist($post1);
+        $manager->persist($post2);
+
+        for ($i = 10; $i <= 20; $i++) {
+            $post = new Post();
+            $post->setMessage('Post '. $i .' Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
+
+            $manager->persist($post);
+        }
+
+        $manager->flush();
+    }
+}
