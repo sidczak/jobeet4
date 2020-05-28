8a31b7f874246310441ae89ca4956ad055a67a4e
sidczak <sidczak@clickmeeting.com>
2020-05-28 15:18:40 +0200

    admin - category page

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Form\Admin\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * Lists all categories entities.
+     *
+     * @Route("/admin/categories", name="admin.category.list", methods="GET")
+     *
+     * @param EntityManagerInterface $em
+     *
+     * @return Response
+     */
+    public function list(EntityManagerInterface $em) : Response
+    {
+        $categories = $em->getRepository(Category::class)->findAll();
+
+        return $this->render('admin/category/list.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * Create category.
+     *
+     * @Route("/admin/category/create", name="admin.category.create", methods="GET|POST")
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     *
+     * @return Response
+     */
+    public function create(Request $request, EntityManagerInterface $em) : Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('admin.category.list');
+        }
+
+        return $this->render('admin/category/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
