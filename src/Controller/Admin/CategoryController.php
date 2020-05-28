5895f4061cb67b7f1c0206222dbccae9bac1f0ae
sidczak <sidczak@clickmeeting.com>
2020-05-28 16:32:27 +0200

    admin - category page btn edit and del

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -59,4 +59,52 @@ class CategoryController extends AbstractController
         ]);
     }
 
+    /**
+     * Edit category.
+     *
+     * @Route("/admin/category/{id}/edit", name="admin.category.edit", methods="GET|POST", requirements={"id" = "\d+"})
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param Category $category
+     *
+     * @return Response
+     */
+    public function edit(Request $request, EntityManagerInterface $em, Category $category) : Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('admin.category.list');
+        }
+
+        return $this->render('admin/category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Delete category.
+     *
+     * @Route("/admin/category/{id}/delete", name="admin.category.delete", methods="DELETE", requirements={"id" = "\d+"})
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param Category $category
+     *
+     * @return Response
+     */
+    public function delete(Request $request, EntityManagerInterface $em, Category $category) : Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $category->getId(), $request->request->get('_token'))) {
+            $em->remove($category);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin.category.list');
+    }
 }
